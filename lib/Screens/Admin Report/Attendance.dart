27159e60bb834a158bb23189b52f7a6dcdb6bb0d
mdwebsoftjpr <mdwebsoftjpr@gmail.com>
2025-05-29 17:16:58 +0530

    sa

--- lib/Screens/Admin Report/Attendance.dart
+++ lib/Screens/Admin Report/Attendance.dart
@@ -98,7 +98,6 @@ class AttendanceState extends State<Attendance> {
   @override
   Widget build(BuildContext context) {
     double deviceWidth = MediaQuery.of(context).size.width;
-    double deviceHeight = MediaQuery.of(context).size.height;
     final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
 
     return Scaffold(
--- lib/Screens/Internet Screen/InternetScreen.dart
+++ lib/Screens/Internet Screen/InternetScreen.dart
@@ -34,7 +34,7 @@ class InternetNotConnectedScreen extends StatelessWidget {
                     ),
                   ); // ✅ Correctly pops this screen
                 },
-                child: Text('Retry'),
+                child: Text('Retry',style: TextStyle(color: Colors.black),),
               ),
             ],
           ),
